f13a63c4f3136058b6600576ebf187293cf6a1bb
Salman Junaidi <sjunaidi.sj@gmail.com>
2018-06-25 23:56:46 -0400

    mongoscraper

---
+++ server.js
@@ -0,0 +1,95 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+// Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/week18day3mongoose");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+
+
+// This will scrape NYTimes
+app.get("/articles", function(req, res) {
+
+  var scrapeData = [];
+  // First, we grab the body of the html with request
+  request("http://www.nytimes.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("a").text();
+      result.link = $(this).children("a").attr("href");
+
+      
+      
+      var entry = new Article(result);
+
+      scrapeData.push(entry);
+
+
+    });  // scraping ends
+
+    res.json(scrapeData); 
+
+  });
+ 
+
+});
+
+// This will grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+  console.log(req.params.id);
+
+});
+
+// Create a scraped article in db
+app.post("/savedarticles/:id", function(req, res) {
+  console.log(req.params.id);
+
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
